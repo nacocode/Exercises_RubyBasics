4d7f5b8ce1ce0361161d26ea380e195f11a74dcc
nacocode <naco120692@gmail.com>
2019-05-06 16:03:01 -0400

    LS:Exercises Ruby Basic:Arrays & Hashes

---
+++ Arrays/MorethanOne.rb
@@ -0,0 +1,5 @@
+pets = ['cat', 'dog', 'fish', 'lizard']
+
+p my_pet = pets[2..3] 
+
+puts "I have a pet #{my_pet[0]} and a pet #{my_pet[1]}!"
---
+++ Hashes/Delete.rb
@@ -0,0 +1,10 @@
+car = {
+  type:    'sedan',
+  color:   'blue',
+  mileage: 80_000,
+  year:    2003
+}
+
+p car
+car.delete(:mileage)
+p car
